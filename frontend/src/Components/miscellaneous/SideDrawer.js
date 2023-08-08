0d2fc62ae48a0751a6f4fc0611dc530e88a61634
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-08-08 19:00:37 +0530

    bb

--- frontend/src/Components/miscellaneous/SideDrawer.js
+++ frontend/src/Components/miscellaneous/SideDrawer.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Box, Text } from "@chakra-ui/layout";
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
-import { Avatar, DrawerContent, DrawerOverlay, Menu, MenuButton, MenuDivider, MenuIcon, MenuItem, MenuList, Tooltip, useDisclosure, Drawer, DrawerHeader, DrawerBody, Input, useToast } from '@chakra-ui/react';
+import { Avatar, DrawerContent, DrawerOverlay, Menu, MenuButton, MenuDivider, MenuIcon, MenuItem, MenuList, Tooltip, useDisclosure, Drawer, DrawerHeader, DrawerBody, Input, useToast, Spinner } from '@chakra-ui/react';
 import { Button } from '@chakra-ui/button';
 import { ChatState } from '../../Context/ChatProvider';
 import ProfileModal from './ProfileModal';
@@ -18,7 +18,7 @@ const SideDrawer = () => {
   const [loading, setLoading] = useState(false);
   const [loadingChat, setLoadingChat] = useState();
 
-  const { user, setSelectedChat } = ChatState();
+  const { user, setSelectedChat, chats, setChats } = ChatState();
   const history = useHistory();
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -48,8 +48,11 @@ const SideDrawer = () => {
         },
       };
       const {data} = await axios.get(`/api/user?search=${search}`, config)
-
-      setLoading(false);
+      if(!chats.find((c) => c._id === data._id)) setSelectedChat(data)
+      {
+        setChats([data, ...chats]);
+      }
+      setLoadingChat(false);
       setSearchResult(data);
     }catch(error){
       toast({
@@ -79,7 +82,14 @@ const SideDrawer = () => {
       setLoading(false);
       onClose();
     } catch (error) {
-      
+      toast({
+        title: "Error fetching the chat",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+    });
     }
   };
   return (
@@ -167,6 +177,7 @@ const SideDrawer = () => {
           ))
         )
       }
+      {loadingChat && <Spinner ml="auto" display="flex" />}
       </DrawerBody>
       </DrawerContent>
     </Drawer>
--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -6,6 +6,7 @@ const ChatContext = createContext();
 const ChatProvider = ({ children }) => {
     const [user, setUser] = useState();
     const [selectedChat, setSelectedChat] = useState();
+    const [chats, setChats] = useState();
 
     const history = useHistory()
 
@@ -20,7 +21,7 @@ const ChatProvider = ({ children }) => {
     }, [history]);
 
     return (
-        <ChatContext.Provider value={{ user, setUser }}>
+        <ChatContext.Provider value={{ user, setUser, selectedChat, setSelectedChat, chats, setChats }}>
             {children}
         </ChatContext.Provider>
     )
