e72f2ed1655d3644527761fe6815f38eda8e551c
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-06 19:56:43 +0530

    chat

--- frontend/src/Components/ScrollableChat.js
+++ frontend/src/Components/ScrollableChat.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ScrollableFeed from 'react-scrollable-feed';
-import { isLastMessage, isSameSender } from '../config/ChatLogics';
+import { isLastMessage, isSameSender, isSameSenderMargin, isSameUser } from '../config/ChatLogics';
 import { ChatState } from '../Context/ChatProvider';
 import { Avatar, Tooltip } from '@chakra-ui/react';
 
@@ -37,7 +37,10 @@ const ScrollableChat = ({ messages }) => {
                         m.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"
                     }`,
                     borderRadius: "20px",
-                    padding: "5px"
+                    padding: "5px 15px",
+                    maxWidth: "75px",
+                    marginLeft: isSameSenderMargin(messages, m, i, user._id),
+                    marginTop: isSameUser(messages, m, i, user._id) ? 3 : 10,
                 }}>
                     {m.content}
                 </span>
