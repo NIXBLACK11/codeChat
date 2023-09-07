510a43d9a99697be4b2a8e05dd5992fe54faeba7
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-09-07 15:38:04 +0530

    socket

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
-const { chats } = require("./data/data");
+// const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
@@ -46,4 +46,34 @@ const io = require('socket.io')(server, {
 
 io.on("connection", (socket) => {
     console.log("connected to socket.io");
+
+    socket.on("setup", (userData) => {
+        socket.join(userData._id);
+        console.log(userData._id);
+        socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+        socket.join(room);
+        console.log("User Joined Room: " + room);
+    });
+
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+    socket.on('new message', (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
+
+        if(!chat.users) return console.log("chat.users not defined");
+
+        chat.users.forEach(user => {
+            if(user._id == newMessageReceived.sender._id) return ;
+            socket.in(user._id).emit("message received", newMessageReceived);
+        });
+
+    });
+    socket.off("setup", () => {
+        console.log("USER DISCONNECTED");
+        socket.leave(userData._id);
+    });
 });
